0e8ed48672c30cd09555b04964d6c503f0a08424
Ayoub-Glizi-Vicioso <ayoub.glizi-vicioso.1@ens.etsmtl.ca>
2024-04-22 10:52:29 -0400

    Revert "Merge branch 'main' of https://github.com/Ayoub-Glizi-Vicioso/TCH099_projet2.0"
    This reverts commit 3eb215f21ac5cfe7a1fa92ad2af9a4c1d0ada581, reversing
    changes made to f5863491c55d2e8f62fa9fb017531a93a6493414.

--- InterfaceWEB/nav_connected.php
+++ InterfaceWEB/nav_connected.php
@@ -1,16 +1,15 @@
-<nav>
-    <div class="nav_logo">
-        <img id="logoImage" src="./images/PalmHaven_Logo.webp" alt="Logo PalmHaven">
-    </div>
-    <ul class="nav__links">
-        <li class="link"><a href="index.php">Accueil</a></li>
-        <li class="link"><a href="Aide.php">Aide</a></li>
-        <li class="link" id="profileLink"><a href="Profilmesreservtion.php">Profil</a></li>
-    </ul>
-</nav>
 
-<script>
-    document.getElementById('logoImage').addEventListener('click', function() {
-        window.location.href = "index.php";
-    });
-</script>
+
+    <nav>
+        <div class="nav_logo">
+            <img src="./images/PalmHaven_Logo.webp" alt="Logo PalmHaven">
+        </div>
+        <ul class="nav__links">
+            <li class="link"><a href="index.php">Accueil</a></li>
+            <li class="link"><a href="Aide.php">Aide</a></li>
+            <li class="link" id="profileLink" ><a href="Profilmesreservtion.php">Profil</a></li>
+        
+        </ul>
+    </nav>
+
+    <br>
