e65cbd533e1902e8d70ee6fb019b98e6bf291f8e
Ayoub-Glizi-Vicioso <ayoub.glizi-vicioso.1@ens.etsmtl.ca>
2024-04-22 21:08:23 -0400

    supprimer compte

--- backendWEB/supprimercompte.js
+++ backendWEB/supprimercompte.js
@@ -21,18 +21,19 @@ $(document).ready(function(){
         
         // Configure la requête DELETE avec l'URL et les paramètres
         xhr.open('DELETE' , url + '?' + params);
+        xhr.setRequestHeader("Content-Type", "application/json");
 
         // Définit une fonction à exécuter lorsque l'état de la requête change
         xhr.onreadystatechange = function (){
             // Vérifie si la requête a abouti avec succès (statut 200)
-            if (xhr.status === 200) {
+            if (xhr.status === 200 && xhr.readyState === XMLHttpRequest.DONE) {
                 // Vous pouvez gérer la réponse en fonction de vos besoins  
                 const data= JSON.parse(xhr.responseText);
                 alert( data['message']); // Affiche un message basé sur la réponse du serveur
                 // Redirige vers une autre page si la suppression du compte réussit
-               // if(data['succes']=='true'){
-              //      window.location.href="../interfaceWEB/index.php";
-               // }
+                if(data['succes']=='true'){
+                    window.location.href="../interfaceWEB/index.php";
+                }
             } else {
                 // Erreur lors de la requête
                 console.error("Erreur lors de la requête : " + xhr.status);
